3c8417c9b7ff7164122d3f217a698c92154aa8a3
Ian Bolton <ibolton@redhat.com>
2024-10-22 14:29:23 -0400

    bump node
    Signed-off-by: Ian Bolton <ibolton@redhat.com>

--- webview-ui/vite.config.ts
+++ webview-ui/vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "build",
     sourcemap: true,
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
       output: {
         entryFileNames: `assets/[name].js`,
         chunkFileNames: `assets/[name].js`,
