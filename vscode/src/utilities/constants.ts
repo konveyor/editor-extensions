adb913be7e27fdacfca3c8a5f07374c4b251b09f
David Zager <david.j.zager@gmail.com>
2025-08-25 15:51:45 -0400

    :sparkles: load config/command namespace from package name
    Signed-off-by: David Zager <david.j.zager@gmail.com>

--- vscode/src/utilities/constants.ts
+++ vscode/src/utilities/constants.ts
@@ -1,7 +1,13 @@
+import { join } from "path";
+import { readFileSync } from "fs";
+
 export const KONVEYOR_SCHEME = "konveyorMemFs";
 export const KONVEYOR_READ_ONLY_SCHEME = "konveyorReadOnly";
 export const RULE_SET_DATA_FILE_PREFIX = "analysis";
 export const PARTIAL_RULE_SET_DATA_FILE_PREFIX = "partial_analysis";
 export const MERGED_RULE_SET_DATA_FILE_PREFIX = "merged_analysis";
 export const SOLUTION_DATA_FILE_PREFIX = "solution";
-export const KONVEYOR_CONFIG_KEY = "konveyor";
+
+const packagePath = join(__dirname, "..", "..", "package.json");
+const packageJson = JSON.parse(readFileSync(packagePath, "utf8"));
+export const EXTENSION_NAME = packageJson.name;
