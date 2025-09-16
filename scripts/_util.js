a689fa56941d49e13a43a7765ab96155f9a9f11f
Ian Bolton <ibolton@redhat.com>
2025-09-16 11:30:50 -0400

    cleanup
    Signed-off-by: Ian Bolton <ibolton@redhat.com>

--- scripts/_util.js
+++ scripts/_util.js
@@ -129,7 +129,7 @@ export function parseCli(
   const ruleset = {
     rulesetOrg: values["ruleset-org"],
     rulesetRepo: values["ruleset-repo"],
-    rulesetReleaseTag: values["ruleset-release-tag"],
+    releaseTag: values["release-tag"],
   };
 
   if (values["use-workflow-artifacts"] && values.workflow && values.pr) {
@@ -150,7 +150,6 @@ export function parseCli(
       repo: values.repo,
       workflow: values.workflow,
       branch: values.branch,
-      releaseTag: values["release-tag"],
       ...ruleset,
     };
   }
